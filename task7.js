dcfe1bb9dcd97c826c58f1e269fcf48c85c2f27d
sphiwe <sphiwe@thedigitalacademy.co.za>
2020-11-13 13:36:39 +0200

    completed task 7

---
+++ task7.js
@@ -0,0 +1,12 @@
+function celsiusToFahrenhiet(celsius){
+    let fahrenhiet = (celsius * 1.8) + 32;
+    return fahrenhiet + " degrees Fahrenhiet";
+}
+
+function fahrenhietToCelsius(fahrenhiet){
+    let celsius = (fahrenhiet / 1.8) - 32;
+    return celsius + " degrees Celsius";
+}
+
+console.log(celsiusToFahrenhiet(1));
+console.log(fahrenhietToCelsius(61));
