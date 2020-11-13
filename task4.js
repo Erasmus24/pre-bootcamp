be6eb7daf4112888e14a8388d8dbde6633eb2d0f
sphiwe <sphiwe@thedigitalacademy.co.za>
2020-11-13 12:49:44 +0200

    refined task 4

--- task4.js
+++ task4.js
@@ -1,11 +1,17 @@
+// function twoNumbers(a, b){
+//     var num = a + b;
+//     var sum = num.toString();
+//     if((a === 3 || b === 3) && (sum.includes("3"))){
+//         return "true";
+//     }else {
+//         return "false";
+//     }
+// }
+
 function twoNumbers(a, b){
-    var num = a + b;
-    var sum = num.toString();
-    if((a === 3 || b === 3) && (sum.includes("3"))){
-        return "true";
-    }else {
-        return "false";
-    }
+    var sum = a + b;
+    var sumString = sum.toString(); //Changing the number to a string
+    return ((a === 3 || b === 3) && (sumString.includes("3"))) ? "true" : "false"
 }
 
-console.log(twoNumbers(50, 3));
+console.log(twoNumbers(3, 1000));
