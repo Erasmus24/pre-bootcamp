bd79c579f100c888a3baee8d4de15e7db4873186
sphiwe <sphiwe@thedigitalacademy.co.za>
2020-11-13 15:53:13 +0200

    completed task 10 like a boss :-)

---
+++ task10.js
@@ -0,0 +1,28 @@
+function vowels(str){
+    var string = '';
+    str = str.toLowerCase().split("");
+    for(j = 0; j <= str.length; j++){
+       switch(str[j]){
+           case "a":
+               string += " a";
+               break;
+            case "e":
+                string += " e";
+                break;
+            case "i":
+                string += " i";
+                break;
+            case "o":
+                string += " o";
+                break;
+            case "u":
+                string += " u";
+                break;
+       }
+       
+    }
+    return string;
+}
+
+console.log(vowels("ERASMUS IS AWESOME !"));
+console.log(vowels("Umuzi Is The Best"));
