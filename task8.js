6ecd449d73b4f992a3aea06136c1dc955092c67c
sphiwe <sphiwe@thedigitalacademy.co.za>
2020-11-13 14:09:26 +0200

    completed task 8

--- task8.js
+++ task8.js
@@ -1,12 +1,12 @@
 function timer(number){
     let hrs = Math.floor(number / 60); //Used Math.floor() to isolate the hour number by removing decimals
     let mins = number % 60; //Used the % to obtain the remainder and converted it into minutes by using 60
-    return (hrs <= 1) ?     // If it's only 1 hour we can't use a plural
+    return (hrs <= 1) ?     // If it's only 1 hour we can't use a plural.
     hrs + " Hour, " + mins + " Minutes" : 
-    hrs + " Hours, " + mins + " Minutes" 
+    hrs + " Hours, " + mins + " Minutes"    //If it's more than 1 hour we can't use a singular.
 
 }
-// return (hrs <= 1) ? hrs + " Hour, " + mins + " Minutes" : hrs + " Hours, " + mins + " Minutes"
+
 
 console.log(timer(71));
 console.log(timer(133));
