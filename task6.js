5800a345a45555ebd98e9e46191f62f11e052315
sphiwe <sphiwe@thedigitalacademy.co.za>
2020-11-17 08:44:49 +0200

    refined task 6

--- task6.js
+++ task6.js
@@ -1,11 +1,18 @@
+// function maximum(a, b, c){
+//     if((a > b && a > c) || a === b){
+//         return a + " is the maximum";
+//     }else if((b > a && b > c) || b === c){
+//         return b + " is the maximum";
+//     }else if((c > b && c > a) || c === a){
+//         return c + " is the maximum";
+//     }
+// }
+
 function maximum(a, b, c){
-    if((a > b && a > c) || a === b){
-        return a + " is the maximum";
-    }else if((b > a && b > c) || b === c){
-        return b + " is the maximum";
-    }else if((c > b && c > a) || c === a){
-        return c + " is the maximum";
-    }
+    return ((a > b && a > c) || a === b) ? a + " is the maximum"
+        : ((b > a && b > c) || b === c) ? b + " is the maximum"
+            : ((c > b && c > a) || c === a) ? c + " is the maximum"
+                : ""
 }
 
-console.log(maximum(19, 19, 19));
+console.log(maximum(419, 219, 219));
