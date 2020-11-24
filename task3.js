7302cf9a9f76baaa9400adda58b6790621cde9d3
sphiwe <sphiwe@thedigitalacademy.co.za>
2020-11-24 21:45:55 +0200

    changed the strings to booleans for task3 and task4

--- task3.js
+++ task3.js
@@ -1,5 +1,5 @@
 function twoNum(a, b) {
-  return a === 65 || b === 65 || a + b === 65 ? "true" : "false";
+  return a === 65 || b === 65 || a + b === 65 ? true : false;
 }
 
 console.log(twoNum(65, 50));
--- task4.js
+++ task4.js
@@ -1,7 +1,7 @@
 function twoNumbers(a, b) {
   var sum = a + b;
   var sumString = sum.toString();
-  return (a === 3 || b === 3) && sumString.includes("3") ? "true" : "false";
+  return (a === 3 || b === 3) && sumString.includes("3") ? true : false;
 }
 
 console.log(twoNumbers(4, 3));
